f338f1974dd61e980b3c1775915e5faf86382931
karlpn <91100704+karlpn@users.noreply.github.com>
2023-11-15 17:56:16 +0100

    modal completed on the specific blog posts page

--- js/script-blog-post.js
+++ js/script-blog-post.js
@@ -25,6 +25,27 @@ async function getApi() {
 function handlePosts(data) {
    document.title = `Food Blog - ${data.title.rendered}`;
    container.innerHTML = `${data.content.rendered}`;
+
+   const image = container.querySelector(".post-image img");
+   image.addEventListener("click", openModal);
+   const modal = document.querySelector("[data-modal]");
+   const modalContainer = document.querySelector(".modal-container");
+
+   function openModal() {
+      modalContainer.innerHTML = `${data.content.rendered}`;
+      modal.showModal();
+   }
+
+   modal.addEventListener("click", function(event) {
+      // Check if the user clicks inside the modal
+      const isOutside = event.target.closest(".modal-container");
+
+      // If not (modal will be = null), close the modal!
+      if(!isOutside) {
+         modalContainer.innerHTML = "";
+         modal.close();
+      }
+   })
 }
 
 getApi();
